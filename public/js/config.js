8ee0d0be7491d4d0c0fd467e63cc314998614fbd
Martin Hanák <martinhanak97@gmail.com>
2023-06-16 12:15:54 +0200

    Initial commit for basic WebRTC app

---
+++ public/js/config.js
@@ -0,0 +1,3 @@
+turnConfig = {
+iceServers: [{   urls: [ "stun:fr-turn1.xirsys.com" ]}, {   username: "wUj2Uii4GXWSQNcS0Pq1Qj5bUDGmfVtu_NxBSnqYoogLRaQjRIGZ3SVhVznNNi_yAAAAAGSMLgZrb2tsZWVraW8=",   credential: "d02ba87c-0c29-11ee-a550-0242ac120004",   urls: [       "turn:fr-turn1.xirsys.com:80?transport=udp",       "turn:fr-turn1.xirsys.com:3478?transport=udp",       "turn:fr-turn1.xirsys.com:80?transport=tcp",       "turn:fr-turn1.xirsys.com:3478?transport=tcp",       "turns:fr-turn1.xirsys.com:443?transport=tcp",       "turns:fr-turn1.xirsys.com:5349?transport=tcp"   ]}]
+}
